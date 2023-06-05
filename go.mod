d0c3c973e04c60326c8f2548c3d3adc961f70618
linch <linch@example.com>
2023-06-05 22:55:15 +0800

    json change to cbor

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bytedance/sonic v1.8.8
+	github.com/fxamacker/cbor/v2 v2.4.0
 	github.com/valyala/fasthttp v1.47.0
 	github.com/valyala/fastjson v1.6.4
 	lukechampine.com/frand v1.4.2
@@ -17,6 +18,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
--- helpers.go
+++ helpers.go
@@ -6,9 +6,8 @@ import (
 
 	"lukechampine.com/frand"
 
-	"github.com/bytedance/sonic"
-	//"github.com/bytedance/sonic/decoder"
-	"github.com/valyala/fastjson"
+	"github.com/fxamacker/cbor/v2"
+	"github.com/valyala/fasthttp"
 )
 
 // func printObject(v interface{}) string {
@@ -18,44 +17,43 @@ import (
 
 // encodeClientRequest encodes parameters for a JSON-RPC client request.
 func encodeClientRequest(method string, args interface{}) ([]byte, error) {
-	c := &clientRequest{
-		ID:      frand.Uint64n(math.MaxUint64),
-		Version: "2.0",
-		Method:  method,
-		Params:  args,
+
+	b, err := cbor.Marshal(args)
+	if err != nil {
+		return b, err
 	}
-	return sonic.Marshal(c)
+
+	arg := &fasthttp.Args{}
+
+	arg.Add("version", "2.0")
+	arg.Add("method", method)
+	arg.Add("id", fmt.Sprintf("%d", frand.Uint64n(math.MaxUint64)))
+	arg.AddBytesV("params", b)
+
+	qs := arg.QueryString()
+
+	return qs, nil
 }
 
 // decodeClientResponse decodes the response body of a client request into the interface reply.
 func decodeClientResponse(method string, r []byte, result interface{}) error {
 
-	val, err := fastjson.ParseBytes(r)
-	if err != nil {
-		err1 := fmt.Errorf("rpc call %s on could not decode body to rpc ParseBytes: %s", method, err.Error())
+	arg := &fasthttp.Args{}
+
+	arg.ParseBytes(r)
+
+	if arg.Has("error") {
+		err1 := fmt.Errorf("rpc call %s on could not decode body to rpc error: %s", method, string(arg.Peek("error")))
 		return err1
 	}
 
-	if val.Exists("error") {
-		err1 := fmt.Errorf("rpc call %s on could not decode body to rpc error: %s", method, string(val.GetStringBytes("error")))
-		return err1
-	}
-
-	if !val.Exists("result") {
+	if !arg.Has("result") {
 		err1 := fmt.Errorf("rpc call %s on could not decode body to rpc response: not found", method)
 		return err1
 	}
 
-	
-	b := val.Get("result").MarshalTo(nil)
-	err = sonic.Unmarshal(b, result)
-	/*
-	ss := string(val.GetStringBytes("result"))
-	decoder := decoder.NewDecoder(ss)
-	decoder.DisallowUnknownFields()
-	decoder.UseNumber()
-	err = decoder.Decode(result)
-	*/
+	b := arg.Peek("result")
+	err := cbor.Unmarshal(b, result)
 	if err != nil {
 		err1 := fmt.Errorf("rpc call %s() on could not decode body to rpc Decode: %s", method, err.Error())
 		return err1
