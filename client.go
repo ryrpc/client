eaee43e01c6f58a759f6944a1b7b73c14a832eac
Carl <87938310+carl9881@users.noreply.github.com>
2024-07-15 21:03:27 +0800

    Update client.go

--- client.go
+++ client.go
@@ -49,10 +49,11 @@ func (cl *Client) makeCallRequest(fctx *fasthttp.RequestCtx, method string, args
 	if len(name) > 1 {
 
 		fctx.Request.Header.CopyTo(&req.Header)
-
+		/*
 		req.Header.Del("Host")
 		req.Header.Del("User-Agent")
 		req.Header.Del("Content-Type")
+  		*/
 		req.Header.Set("func", name[1])
 	}
 
